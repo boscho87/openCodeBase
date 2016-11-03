07c5563cd6572313ab71f159917b69e7851e1cea
Simon Müller <simon.d.mueller@gmail.com>
2016-11-03 22:30:31 +0100

    Update modulePattern.js

--- javascript/modulePattern.js
+++ javascript/modulePattern.js
@@ -24,3 +24,40 @@ testModule.incrementCounter();
 // Check the counter value and reset
 // Outputs: counter value prior to reset: 1
 testModule.resetCounter();
+
+
+
+
+
+
+
+var myNamespace = (function () {
+ 
+  var myPrivateVar, myPrivateMethod;
+ 
+  // A private counter variable
+  myPrivateVar = 0;
+ 
+  // A private function which logs any arguments
+  myPrivateMethod = function( foo ) {
+      console.log( foo );
+  };
+ 
+  return {
+ 
+    // A public variable
+    myPublicVar: "foo",
+ 
+    // A public function utilizing privates
+    myPublicFunction: function( bar ) {
+ 
+      // Increment our private counter
+      myPrivateVar++;
+ 
+      // Call our private method using bar
+      myPrivateMethod( bar );
+ 
+    }
+  };
+ 
+})();
