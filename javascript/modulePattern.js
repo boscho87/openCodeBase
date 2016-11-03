f8d5ba87da5853c710e2268c3f1177f565ec1a38
Simon Müller <simon.d.mueller@gmail.com>
2016-11-03 22:32:21 +0100

    Update modulePattern.js

--- javascript/modulePattern.js
+++ javascript/modulePattern.js
@@ -61,3 +61,83 @@ var myNamespace = (function () {
   };
  
 })();
+
+
+
+
+// basketModule returns an object with a public API we can use
+ 
+basketModule.addItem({
+  item: "bread",
+  price: 0.5
+});
+ 
+basketModule.addItem({
+  item: "butter",
+  price: 0.3
+});
+ 
+// Outputs: 2
+console.log( basketModule.getItemCount() );
+ 
+// Outputs: 0.8
+console.log( basketModule.getTotal() );
+ 
+// However, the following will not work:
+ 
+// Outputs: undefined
+// This is because the basket itself is not exposed as a part of our
+// public API
+console.log( basketModule.basket );
+ 
+// This also won't work as it only exists within the scope of our
+// basketModule closure, but not in the returned public object
+console.log( basket );
+
+
+
+
+
+// Global module
+var myModule = (function ( jQ, _ ) {
+ 
+    function privateMethod1(){
+        jQ(".container").html("test");
+    }
+ 
+    function privateMethod2(){
+      console.log( _.min([10, 5, 100, 2, 1000]) );
+    }
+ 
+    return{
+        publicMethod: function(){
+            privateMethod1();
+        }
+    };
+ 
+// Pull in jQuery and Underscore
+})( jQuery, _ );
+ 
+myModule.publicMethod();
+
+
+
+// Global module
+var myModule = (function () {
+ 
+  // Module object
+  var module = {},
+    privateVariable = "Hello World";
+ 
+  function privateMethod() {
+    // ...
+  }
+ 
+  module.publicProperty = "Foobar";
+  module.publicMethod = function () {
+    console.log( privateVariable );
+  };
+ 
+  return module;
+ 
+})();
