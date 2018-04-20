e3c6eab467352d77f09680a3c9120a510fbbb9e2
Simon Müller <simon.d.mueller@gmail.com>
2018-04-20 08:34:01 +0200

    Update post-receive.sh

--- git/post-receive.sh
+++ git/post-receive.sh
@@ -7,7 +7,7 @@ do
         git --work-tree=/var/www/html/ --git-dir=/var/www/.git checkout 
     elif  [[ $ref =~ .*/stage$ ]];
     then
-        echo "Stage ref received.  Deploying stage branch to production..."
+        echo "Stage ref received.  Deploying stage branch to stage..."
         git --work-tree=/var/www/html/ --git-dir=/var/www/.git checkout stage
     else
         echo "Ref $ref successfully received.  Doing nothing: only the master branch may be deployed on this server."
