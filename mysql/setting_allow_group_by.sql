a1474a3a6c297551a4e42c5f48760a77a5cd7752
Simon Müller <simon.d.mueller@gmail.com>
2018-08-15 10:18:15 +0200

    Update setting_allow_group_by.sql

--- mysql/setting_allow_group_by.sql
+++ mysql/setting_allow_group_by.sql
@@ -1,1 +1,6 @@
 SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+
+# oder `sudo nano /etc/mysql/my.cnf` und die folgenden 2 zeilen einfügen
+[mysqld]  
+sql_mode = "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
