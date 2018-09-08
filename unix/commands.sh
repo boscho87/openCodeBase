8c12cde93b3a88b0f7ef412944a2f2ea531bfae2
Simon Müller <simon.d.mueller@gmail.com>
2018-09-08 15:01:51 +0200

    Update commands.sh

--- unix/commands.sh
+++ unix/commands.sh
@@ -1,3 +1,11 @@
 
 # replace http:// with https://
 egrep -lRZ 'http://' test.sql | xargs -0 -l sed -i -e 's$http://$https://$g'
+
+
+
+# rasberry pi usb on/off
+# OFF
+echo '1-1' |sudo tee -a /sys/bus/usb/drivers/usb/unbind
+# ON
+echo '1-1' |sudo tee -a /sys/bus/usb/drivers/usb/bind
